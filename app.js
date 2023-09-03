e3d2bee131fc391899f17f595827a517dee03111
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-09-03 23:23:26 +0600

    O=> assignment released version (submission)

--- app.js
+++ app.js
@@ -8,8 +8,4 @@ app.use(express.json());
 // Routes
 app.use('/api', apiRouter);
 
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
